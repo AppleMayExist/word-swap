c0dd2f6f1cd4ddf6d0a1762473830372c743a8de
AppleMayExist <62101291+AppleMayExist@users.noreply.github.com>
2021-08-25 08:23:01 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ addEventListener("fetch", async (event) => {
   event.respondWith(
     new Response(await (await fetch("https://raw.githubusercontent.com/AppleMayExist/word-swap/main/wordSwap.html")).text(), {
       status: 200,
-      headers: { "content-type": "text/plain" },
+      headers: { "content-type": "text/html" },
     }),
    );
 });
