ad1e34af51bc15b9f6ca7efabea90d93f66f4683
AppleMayExist <62101291+AppleMayExist@users.noreply.github.com>
2021-08-25 08:30:58 +1000

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-
-addEventListener("fetch", async (event) => {
-  event.respondWith(
-    new Response(await (await fetch("https://raw.githubusercontent.com/AppleMayExist/word-swap/main/wordSwap.html")).text(), {
-      status: 200,
-      headers: { "content-type": "text/html" },
-    }),
-   );
-});
